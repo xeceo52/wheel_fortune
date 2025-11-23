4398ec492efbbccea12e25fb3c038a01746f5d8b
xeceo52 <xco.vvv@gmail.com>
2025-11-24 01:25:08 +0500

    изменили путь до public

--- wheel_fortune/config/filesystems.php
+++ wheel_fortune/config/filesystems.php
@@ -75,7 +75,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        base_path('public_html') => base_path('wheel_fortune/public'),
+        base_path('public_html') => base_path('public'),
     ],
 
 ];
